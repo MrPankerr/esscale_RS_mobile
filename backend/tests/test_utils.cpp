7d8449fa8e5024c23ce2e571a5072d75cc7cdd6f
Vsevolod Pyatkov <MrPankerr@gmail.com>
2025-04-13 18:28:15 +0300

    fixed CMakeLists

--- backend/tests/test_utils.cpp
+++ backend/tests/test_utils.cpp
@@ -12,8 +12,3 @@ TEST(UtilsTest, HelloOutput) {
     std::string output = testing::internal::GetCapturedStdout();
     EXPECT_EQ(output, "Hello from backend!\n");
 }
-
-int main(int argc, char** argv) {
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
